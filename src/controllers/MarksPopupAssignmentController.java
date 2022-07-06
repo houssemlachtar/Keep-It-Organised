47833e7bd6e3dc8ce6f0b2d2f2e7d7054e602511
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 17:11:45 +0100

    Update MarksPopupAssignmentController.java

--- src/controllers/MarksPopupAssignmentController.java
+++ src/controllers/MarksPopupAssignmentController.java
@@ -15,17 +15,7 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Controller for Assignment popup in the Marks tab.
- * Used when a Assignment is selected and added, edited or deleted.
- */
+
 public class MarksPopupAssignmentController extends MarksDefaultPopup implements Initializable {
     // Label of the scene's title
     @FXML
