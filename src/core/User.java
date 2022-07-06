6df82a8d009632b54a1194ef9db27e6d3189025c
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 04:08:05 +0100

    Update User.java

--- src/core/User.java
+++ src/core/User.java
@@ -15,16 +15,7 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Class representing a user in the system.
- */
+
 public class User {
     private String forename;
     private String username;
