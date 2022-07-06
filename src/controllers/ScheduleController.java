615b90191d7112149f91f24c82752a91638d116e
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 17:14:20 +0100

    Update ScheduleController.java

--- src/controllers/ScheduleController.java
+++ src/controllers/ScheduleController.java
@@ -27,16 +27,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.ResourceBundle;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Class which handles Schedule Tab functionality and UI.
- */
+
 public class ScheduleController extends DefaultNavigation implements Initializable {
 
     // Top panel fields
