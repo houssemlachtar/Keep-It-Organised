cb52c42609d3267456d77873da92caf976a388c3
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 17:21:35 +0100

    Update TimePeriodController.java

--- src/controllers/TimePeriodController.java
+++ src/controllers/TimePeriodController.java
@@ -33,16 +33,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Class which handles Time Tab functionality and UI when a Period is selected.
- */
+
 public class TimePeriodController extends DefaultNavigation implements Initializable {
     // Top information pane elements
     @FXML
