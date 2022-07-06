3e5dd2b589f03bbdfe9a06acd06f857be370ec14
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 17:10:28 +0100

    Update AboutController.java

--- src/controllers/AboutController.java
+++ src/controllers/AboutController.java
@@ -12,16 +12,7 @@ import javafx.scene.layout.Pane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Class which handles About Tab functionality and UI.
- */
+
 public class AboutController extends DefaultNavigation implements Initializable {
     @FXML
     private Label topicNameLabel;
