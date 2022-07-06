7243336259c1783dcfd216a84747f6f310c3e368
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 04:04:09 +0100

    Update MarksSelection.java

--- src/core/enums/MarksSelection.java
+++ src/core/enums/MarksSelection.java
@@ -1,16 +1,6 @@
 package core.enums;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Enum representing Marks Tab selections, which determine
- * the displayed information.
- */
+
 public enum MarksSelection {
 
     DEGREE("Degree"),
