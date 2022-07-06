82894b5d1c58b993d79277b9185f9d605bd674a6
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 17:21:11 +0100

    Update TimeController.java

--- src/controllers/TimeController.java
+++ src/controllers/TimeController.java
@@ -32,16 +32,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.ResourceBundle;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Class which handles Time Tab functionality and UI.
- */
+
 public class TimeController extends DefaultNavigation implements Initializable {
 
     // Top pane values
