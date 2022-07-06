2c5e9c940f4fa7fea90bdb2b4a7be150ee8e5ade
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 04:06:53 +0100

    Update Day.java

--- src/core/Day.java
+++ src/core/Day.java
@@ -15,16 +15,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Class to represent a Day in the system.
- */
+
 public class Day {
     private final int id;
     private final int userId;
