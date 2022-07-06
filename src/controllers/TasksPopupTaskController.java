9c43fc7520af62f9a8d90e52bd172f58041ad1b2
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 17:20:32 +0100

    Update TasksPopupTaskController.java

--- src/controllers/TasksPopupTaskController.java
+++ src/controllers/TasksPopupTaskController.java
@@ -20,17 +20,7 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Controller for Task popup in the Tasks tab.
- * Used when a Task is added, edited or deleted.
- */
+
 public class TasksPopupTaskController extends DefaultNavigation implements Initializable {
     // Popup label
     @FXML
