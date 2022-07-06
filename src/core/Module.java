f6646da9410aac47e514cf438acc6a5819ad3f06
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-06 04:07:20 +0100

    Update Module.java

--- src/core/Module.java
+++ src/core/Module.java
@@ -12,16 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * Organised.
- * Copyright (c) 2021, Agne Knietaite
- * All rights reserved.
- *
- * This source code is licensed under the GNU General Public License, Version 3
- * found in the LICENSE file in the root directory of this source tree.
- *
- * Class for representing a module in the system
- */
+
 public class Module {
     private final int id;
     private final int userId;
